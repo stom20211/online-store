f90c4242545214063129c765094cc3c4c3ea4771
stom20211 <94426810+stom20211@users.noreply.github.com>
2021-11-29 10:25:36 -0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,20 @@
+let mytime=setInterval(timeFunction,1000);
 
-<script>
-let currentTime=new Date();
-let myhour=currentTime.getHours;
-document.getElementById("myid").innerHTML = "Hello World";
-</script>
+function timeFunction(){
+    let currentTime=new Date();
+    let currentHour=currentTime.getHours();
+    let currentMinute=currentTime.getMinutes();
+    let currentSecond=currentTime.getSeconds();
+    let message1="Good Morning!";
+    let message2="Good Afternoon!";
+    let message3="Good Evening!";
+    if(currentHour<13){
+        document.getElementById("myid").innerHTML = `${message1} The current time is ${currentHour}:${currentMinute}:${currentSecond} AM.`;
+    }
+    else if(currentHour<18){
+        document.getElementById("myid").innerHTML = `${message2} The current time is ${currentHour-12}:${currentMinute}:${currentSecond} PM.`;  
+    }
+    else{
+        document.getElementById("myid").innerHTML = `${message3} The current time is ${currentHour-12}:${currentMinute}:${currentSecond} PM.`;
+    }
+}
